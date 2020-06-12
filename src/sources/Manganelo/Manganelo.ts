5119073041f71aa146f3e978371019a3db930022
Conrad <vibrantclouds@protonmail.com>
2020-06-12 16:52:21 -0400

    Function refactoring

--- src/sources/Manganelo/Manganelo.ts
+++ src/sources/Manganelo/Manganelo.ts
@@ -17,7 +17,7 @@ export class Manganelo extends Source {
     super(cheerio)
   }
 
-  get version(): string { return '1.1.0' }
+  get version(): string { return '1.1.2' }
   get name(): string { return 'Manganelo' }
   get icon(): string { return 'icon.png' }
   get author(): string { return 'Daniel Kovalevich' }
@@ -425,19 +425,7 @@ export class Manganelo extends Source {
    * @param request
    */
   requestModifier(request: Request): Request { 
-    
-    let headers: any = request.headers == undefined ? {} : request.headers
-    headers['Referer'] = `${MN_DOMAIN}`
-
-    return createRequestObject({
-      url: request.url,
-      method: request.method,
-      headers: headers,
-      data: request.data,
-      timeout: request.timeout,
-      param: request.param,
-      cookies: request.cookies,
-      incognito: request.incognito
-    })
+    request.headers == undefined ? {} : request.headers["Referer"] = `${MN_DOMAIN}`
+    return request
   }
 }
--- src/sources/WebToons/WebToons.ts
+++ src/sources/WebToons/WebToons.ts
@@ -18,7 +18,7 @@ export class WebToons extends Source {
     super(cheerio)
   }
 
-  get version(): string { return '0.2.0' }
+  get version(): string { return '0.2.1' }
   get name(): string { return 'WebToons (BETA)' }
   get description(): string { return 'Extension that pulls comics from WebToons' }
   get author(): string { return 'Conrad Weiser' }
@@ -221,19 +221,8 @@ export class WebToons extends Source {
   }
 
   requestModifier(request: Request): Request { 
-    
-    let headers: any = request.headers == undefined ? {} : request.headers
-    headers['Referer'] = `${WEBTOONS_SEARCH_DOMAIN}`
-
-    return createRequestObject({
-      url: request.url,
-      method: request.method,
-      headers: headers,
-      data: request.data,
-      timeout: request.timeout,
-      param: request.param,
-      cookies: request.cookies,
-      incognito: request.incognito
-    })
+    request.headers == undefined ? {} : request.headers["Referer"] = `${WEBTOONS_SEARCH_DOMAIN}`
+    return request
   }
 }
+
