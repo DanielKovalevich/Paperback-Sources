73065a6a1b37336ee16d5caf893a561bf930e248
Shezan Shiraz <shezanshiraz@gmail.com>
2021-03-25 11:32:10 +0530

    Changed Stuff to Promises

--- src/MangaOwl/MangaOwl.ts
+++ src/MangaOwl/MangaOwl.ts
@@ -13,9 +13,11 @@ import {
     TagType,
 } from "paperback-extensions-common"
 
+import {Parser,} from './Parser'
+
 const MANGAOWL_DOMAIN = 'https://mangaowl.net/'
 
-export const ReadComicsToInfo: SourceInfo = {
+export const MangaOwlInfo: SourceInfo = {
     version: '1.0.0',
     name: 'MangaOwl',
     description: 'Extension that pulls manga from MangaOwl, includes Advanced Search and Updated manga fetching',
@@ -30,145 +32,236 @@ export const ReadComicsToInfo: SourceInfo = {
 
 export class MangaOwl extends Source {
 
-    
-
-    constructor(cheerio: CheerioAPI) {
-        super(cheerio)
-    }
-
     parser = new Parser()
 
-
+    
     getMangaShareUrl(mangaId: string): string | null {
-        return `${MANGAOWL_DOMAIN}/Comic/${mangaId}`
+        return `${MANGAOWL_DOMAIN}/single/${mangaId}`
     }
 
 
 
     get rateLimit() { return 100 }
-    getMangaDetailsRequest(ids: string[]): Request[] {
-        return [createRequestObject({
-            metadata: ids[0],
-            url: "https://mangaowl.net/single/" + ids[0] + "/pp",
-            method: 'GET'
-        })]
-    }
-    getMangaDetails(data: any, metadata: any): Manga[] {
-        let $ = this.cheerio.load(data)
-        console.log(data)
-        let mangaInfo = $(".single_detail")
-        let mangaProps = $('div:nth-child(2) p', mangaInfo).toArray()
-        let mangaIdRegex = /(?:single\/)(\w+)/gi
-        let manga = {
-            id: metadata,
-            titles: [$('h2', mangaProps).text()],
-            image: $('img', mangaInfo).attr('data-src')!.replace(/^(\/\/)/gi, 'https://'),
-            rating: 0,
-            status: MangaStatus.ONGOING,
-            artist: "",
-            author: "",
-            desc: $('.single_detail .description').text().replace(/^\./gi, ''),
-        }
-        for (let prop of mangaProps) {
-            let propertyValue = $(prop).text().toLowerCase()
 
-            if (propertyValue.includes('rating')) {
-                manga.rating = Number($('font', prop).text())
-            } else if (propertyValue.includes('status')) {
-                let status = $(prop).contents().filter((_, x) => {
-                    return x.type === 'text';
-                }).text();
-                manga.status = status.toLowerCase().includes('ongoing') ? MangaStatus.ONGOING : MangaStatus.COMPLETED
-            } else if (propertyValue.includes('author')) {
-                let author = $(prop).contents().filter((_, x) => {
-                    return x.type === 'text';
-                }).text();
-                manga.author = author.trim()
-            } else if (propertyValue.includes('artist')) {
-                let artist = $(prop).contents().filter((_, x) => {
-                    return x.type === 'text'
-                }).text();
-                manga.artist = artist.trim()
-            }
-        }
-        return [createManga(manga)]
-    }
-    getChaptersRequest(mangaId: string): Request {
-        return createRequestObject({
-            metadata: mangaId,
-            url: "https://mangaowl.net/single/" + mangaId+ "/chapters-or-something",
-            method: 'GET'
-        })
-    }
-    getChapters(data: any, metadata: any): Chapter[] {
-        let $ = this.cheerio.load(data)
-        let chapters = $('.table-chapter-list .list-group-item.chapter_list').toArray()
-        let chapterList = []
-        let chapterStrRegex = /(chapter)\s?(\d+\.?\d+)/gim
-        let chapterNumberRegex = /(\d+?)\.?(\d+$)/gim
-                for (let chapter of chapters) {
-            let chapNum = $('label.chapter-title', chapter).text().match(chapterStrRegex)![0].match(chapterNumberRegex)!
-            // paste stuff here from now on
-            chapterList.push(createChapter({
-                id: $('.chapter-url', chapter).attr('chapter-id')!,
-                chapNum: Number(chapNum[0]),
-                langCode: LanguageCode.ENGLISH,
-                volume: 0,
-                mangaId: metadata,
-                name: "",
-                time: new Date($('.chapter-url small', chapter).text()),
-            }))
-        }
-        return chapterList
-    }
-    searchRequest(query: SearchRequest, page: number): Request | null {
-        return createRequestObject({
-            url: "https://mangaowl.net/search/1?search=" + encodeURI(query.title ?? ""),
-            method: "GET"
-        })
-    }
-    search(data: any, metadata: any): MangaTile[] | null {
-        let $ = this.cheerio.load(data)
-        let searchResults = $('.flexslider .comicView').toArray()
-        let mangaIdRegex = /(?:single\/)(\w+)/gi 
-        let mangas = []
-        for (let result of searchResults) {
-            mangas.push(createMangaTile({
-                id: $('a', result).attr('href')!.match(mangaIdRegex)![0].split('/')[1],
-                image: $('.comic_thumbnail', result).attr('data-background-image')!,
-                title: createIconText({ text: $('.comic_title', result).text().trim() }),
-            
-            }))
-        }
+    async getMangaDetails(mangaId: string): Promise<Manga> {
 
-        return mangas
-    }
-    getChapterDetailsRequest(mangaId: string, chapId: string): Request {
-        return createRequestObject({
-            url: "http://mangaowl.net/reader/" + mangaId + "/" + chapId,
+        let request = createRequestObject({
+            url: `${MANGAOWL_DOMAIN}/single/${mangaId}`,
             method: 'GET',
-            metadata: {
-                chapId: chapId,
-                mangaId: mangaId
-            }
+            
         })
+        const data = await this.requestManager.schedule(request, 1)
+
+        let $ = this.cheerio.load(data.data)
+        
+
+        return this.parser.parseMangaDetails($, mangaId)
     }
 
-    getChapterDetails(data: any, metadata: any): ChapterDetails {
-        let $ = this.cheerio.load(data, { xmlMode: false })
 
-        let allPages = $('#reader img[data-src]').toArray()
-        let pages = []
-        for (let page of allPages) {
-            pages.push($(page).attr('data-src')!)
-        }
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        let request = createRequestObject({
+            url: `${MANGAOWL_DOMAIN}/single/${mangaId}`,
+            method: "GET",
+            
+        })
+
+        const data = await this.requestManager.schedule(request, 1)
+        let $ = this.cheerio.load(data.data)
+
+        let chapters = this.parser.parseChapterList($, mangaId)
+
+        return chapters
+    }
+
+
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+
+        let request = createRequestObject({
+            url: `${MANGAOWL_DOMAIN}/single/${mangaId}`,
+            method: 'GET',
+            param: '?readType=1&quality=hq',
+            
+        })
+
+        let data = await this.requestManager.schedule(request, 1)
+
+        let $ = this.cheerio.load(data.data)
+        let pages = this.parser.parseChapterDetails(data.data,this.cheerio)
+
+
+
 
         return createChapterDetails({
-            id: metadata.chapId,
-            longStrip: false,
-            mangaId: metadata.mangaId,
-            pages: pages
+            id: chapterId,
+            mangaId: mangaId,
+            pages: pages,
+            longStrip: false
         })
     }
 
+
+
+    async searchRequest(query: SearchRequest, metadata: any, ): Promise<PagedResults> {
+        let page: number = metadata?.page ?? 1
+
+        let request = this.constructSearchRequest(query.title??'')
+
+
+
+        let data = await this.requestManager.schedule(request, 1)
+        let $ = this.cheerio.load(data.data)
+        let manga = this.parser.parseSearchResults($,this.cheerio)
+        let mData = undefined
+        if (!this.parser.isLastPage($)) {
+            mData = {page: (page + 1)}
+        }
+
+        return createPagedResults({
+            results: manga,
+            metadata: mData
+        })
+
+    }
+
+
+    // async getTags(): Promise<TagSection[] | null> {
+    //     const request = createRequestObject({
+    //         url: `${READCOMICSTO_DOMAIN}/comic-genres/`,
+    //         method: 'GET'
+    //     })
+
+    //     const data = await this.requestManager.schedule(request, 1)
+    //     let $ = this.cheerio.load(data.data)
+
+    //     return this.parser.parseTags($)
+    // }
+
+
+    async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+
+        const sections = [
+            {
+                request: createRequestObject({
+                    url: `${MANGAOWL_DOMAIN}/ComicList/Newest`,
+                    method: 'GET',
+                   
+                }),
+                section: createHomeSection({
+                    id: '0',
+                    title: 'NEWEST COMICS',
+                    view_more: true
+                }),
+            },
+            {
+                request: createRequestObject({
+                    url: `${MANGAOWL_DOMAIN}/ComicList/LatestUpdate`,
+                    method: 'GET',
+                    
+                }),
+                section: createHomeSection({
+                    id: '1',
+                    title: 'RECENTLY UPDATED',
+                    view_more: true,
+                }),
+            },
+            {
+                request: createRequestObject({
+                    url: `${MANGAOWL_DOMAIN}/ComicList/MostPopular`,
+                    method: 'GET',
+                    
+                }),
+                section: createHomeSection({
+                    id: '2',
+                    title: 'MOST POPULAR',
+                    view_more: true,
+                }),
+            },
+        ]
+
+        const promises: Promise<void>[] = []
+
+        for (const section of sections) {
+            // Let the app load empty sections
+            sectionCallback(section.section)
+
+            // Get the section data
+            promises.push(
+                this.requestManager.schedule(section.request, 1).then(response => {
+                    const $ = this.cheerio.load(response.data)
+                    section.section.items = this.parser.parseSearchResults($, this.cheerio)
+                    sectionCallback(section.section)
+                }),
+            )
+        }
+
+        // Make sure the function completes
+        await Promise.all(promises)
+    }
+
+
+    async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults | null> {
+        let webPage = ''
+        let page: number = metadata?.page ?? 1
+        switch (homepageSectionId) {
+            case '0': {
+                webPage = `/ComicList/Newest?page=${page}`
+                break
+            }
+            case '1': {
+                webPage = `/ComicList/LatestUpdate?page=${page}`
+                break
+            }
+            case '2': {
+                webPage = `/ComicList/MostPopular?page=${page}`
+                break
+            }
+            default:
+                return Promise.resolve(null)
+        }
+
+        let request = createRequestObject({
+            url: `${MANGAOWL_DOMAIN}${webPage}`,
+            method: 'GET',
+            
+        })
+
+        let data = await this.requestManager.schedule(request, 1)
+        let $ = this.cheerio.load(data.data)
+        let manga = this.parser.parseHomePageSection($,this.cheerio)
+        let mData
+        if (!this.parser.isLastPage($)) {
+            mData = {page: (page + 1)}
+        } else {
+            mData = undefined  // There are no more pages to continue on to, do not provide page metadata
+        }
+
+        return createPagedResults({
+            results: manga,
+            metadata: mData
+        })
+    }
+    
+
+
+
+    CloudFlareError(status: any) {
+        if(status == 503) {
+            throw new Error('CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > \<\The name of this source\> and press Cloudflare Bypass')
+        }
+    }
+
+
+    constructSearchRequest(searchQuery: string): any {
+        let isSearch = searchQuery != ''
+        let data: any = {
+            "keyword": searchQuery,
+        }
+
+        return createRequestObject({
+            url: `${MANGAOWL_DOMAIN}/Search/Comic`,
+            method: 'POST',
+            data: this.urlEncodeObject(data),
+        })
+    }
 }
