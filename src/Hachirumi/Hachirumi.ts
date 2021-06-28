5e32651f8f32e1ef5dce18b79ed464e7224ac22a
Rachala Ovin <resistlyfe@gmail.com>
2021-06-28 16:53:38 +0530

    Added `getChapters` and fixed `getMangaDetails`
    Forgot about the `Promise<Manga>`.

--- src/Hachirumi/Hachirumi.ts
+++ src/Hachirumi/Hachirumi.ts
@@ -28,7 +28,11 @@ export const HachirumiInfo: SourceInfo = {
 };
 
 export class Hachirumi extends Source {
-  async getMangaDetails(mangaId: string): Promise<any> {
+  /* 
+  Though "mangaId" is mentioned here Hachirumi uses slugs. 
+  eg: the-story-about-living
+  */
+  async getMangaDetails(mangaId: string): Promise<Manga> {
     let request = createRequestObject({
       url: HACHIRUMI_API + "/series/" + mangaId,
       method: "GET",
@@ -45,9 +49,9 @@ export class Hachirumi extends Source {
 
     return createManga({
       id: result.slug,
-      titles: result.title,
+      titles: [result.title],
       image: HACHIRUMI_DOMAIN + result.cover,
-      rating: 5,
+      rating: 0, // Rating is not supported by Hachirumi.
       status: MangaStatus.ONGOING,
       artist: result.artist,
       author: result.author,
@@ -55,8 +59,48 @@ export class Hachirumi extends Source {
     });
   }
 
-  getChapters(mangaId: string): Promise<Chapter[]> {
-    throw new Error("Method not implemented.");
+  /*
+  Follows the same format as `getMangaDetails`.
+  Hachirumi serves both chapters and manga in single request.
+  */
+  async getChapters(mangaId: string): Promise<Chapter[]> {
+    let request = createRequestObject({
+      url: HACHIRUMI_API + "/series/" + mangaId,
+      method: "GET",
+      headers: {
+        "accept-encoding": "application/json",
+      },
+    });
+
+    let response = await this.requestManager.schedule(request, 1);
+    let result =
+      typeof response.data === "string" || typeof response.data !== "object"
+        ? JSON.parse(response.data)
+        : response.data;
+
+    let chapterObject = result["chapters"];
+    let groupObject = result["groups"];
+    let chapters = [];
+
+    for (let key in chapterObject) {
+      let metadata = chapterObject[key];
+
+      for (let groupKey in metadata["groups"]) {
+        chapters.push(
+          createChapter({
+            id: metadata["folder"],
+            mangaId: result.slug,
+            chapNum: parseInt(key),
+            langCode: LanguageCode.ENGLISH,
+            name: metadata["title"],
+            volume: metadata["volume"],
+            group: groupObject[groupKey],
+            time: new Date(metadata["release_date"][groupKey] * 1000),
+          })
+        );
+      }
+    }
+    return chapters;
   }
   getChapterDetails(
     mangaId: string,
